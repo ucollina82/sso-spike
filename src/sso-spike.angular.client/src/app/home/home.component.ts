e9cfd7ef15cb415775281bbe99fe9ea1bd925602
ucollina82 <ucollina82@gmail.com>
2023-04-14 12:36:32 +0200

    added user label to home page

--- src/sso-spike.angular.client/src/app/home/home.component.ts
+++ src/sso-spike.angular.client/src/app/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from 'src/oidc/services';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  userInfo: string = '';
+
+  constructor(readonly authService: AuthService) {
+
+  }
+
+  ngOnInit(): void {
+    this.authService.getUserInfo().then(user => {this.userInfo = JSON.stringify(user)});
+  }
 }
